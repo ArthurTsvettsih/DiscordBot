78f8546614fbf07b41710e714c0402b6989010d7
Arthur Tsvettsih <arthur.tsvettsih@nomuda.com>
2018-04-23 17:42:32 +0100

    # Added help command

---
+++ DiscordBot/Modules/Help.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+	public class Help : ModuleBase<SocketCommandContext>
+	{
+
+		[Command("help")]
+		public async Task HelpAsync()
+		{
+			EmbedBuilder embedBuilder = new EmbedBuilder();
+
+			embedBuilder
+				.AddField("ab!help", "Displays a list of available commands with examples")
+				.AddField("ab!weather London", "Gets the weather information for the specified city")
+				.AddField("ab!raffle everyone", "Chooses a random person with the specified role");
+
+			await ReplyAsync("", false, embedBuilder.Build());
+		}
+	}
+}
